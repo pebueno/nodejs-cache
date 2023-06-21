d71e38699b4bb5f3902e287ba958b71c3010a0a9
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2023-06-20 22:10:18 -0300

    Display repos

--- app.js
+++ app.js
@@ -2,32 +2,42 @@ const express = require('express');
 const redis = require('redis');
 const fetch = require('node-fetch');
 
-const app = express();
-
-// Make request to GitHub for Data
-async function getRepos(req, res, next) {
-    try {
-        console.log('Fetching Data...');
-
-        const { username } = req.params;
-
-        const response = await fetch(`https://api.github.com/users/${username}`);
-
-        const data = await response.json();
-
-        res.send(data);
-    } catch (err) {
-        console.log(err);
-        res.status(500);
-    }
-}
-
-app.get('/repos/:username', getRepos);
-
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+// Set response
+function setResponse(username, repos) {
+  return `<h2>${username} has ${repos} GitHub repos</h2>`;
+}
+
+// Make request to GitHub for Data
+async function getRepos(req, res, next) {
+  try {
+    console.log('Fetching Data...');
+
+    const { username } = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await response.json();
+
+    const repos = data.public_repos;
+
+    // Set data to Redis
+    client.setex(username, 3600, repos);
+
+    res.send(setResponse(username, repos));
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+};
+
+app.get('/repos/:username', getRepos);
 
 app.listen(PORT, () => {
   console.log(`Access server on  http://localhost:${PORT}`);
