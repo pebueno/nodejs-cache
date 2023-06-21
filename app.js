2c6f6027cc69d984d86868fbcc52ca2534869506
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2023-06-20 21:48:50 -0300

    Install Redis

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const redis = require('redis');
+const fetch = require('node-fetch');
 
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+// Make request to GitHub for Data
+async function getRepos(req, res, next) {
+    try {
+        console.log('Fetching Data...');
 
-const port = 3000;
+        const { username } = req.params;
 
-app.listen(port, () => {
-  console.log(`Access server on  http://localhost:${port}`);
+        const response = await fetch(`https://api.github.com/users/${username}`);
+
+        const data = await response.json();
+
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+    }
+}
+
+app.get('/repos/:username', getRepos);
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+app.listen(PORT, () => {
+  console.log(`Access server on  http://localhost:${PORT}`);
 });
